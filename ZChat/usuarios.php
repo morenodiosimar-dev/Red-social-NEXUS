cc55774262160cab595336fc5f6e6647879deae1
morenodiosimar-dev <morenodiosimar@gmail.com>
2026-02-10 19:31:59 -0400

    Ojala

--- ZChat/usuarios.php
+++ ZChat/usuarios.php
@@ -10,6 +10,7 @@ $allowed_origins = [
 if (in_array($origin, $allowed_origins)) {
     header("Access-Control-Allow-Origin: $origin");
 }
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");

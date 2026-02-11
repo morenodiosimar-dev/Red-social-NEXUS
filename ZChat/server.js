122c117877f2bca10beb673434667be46b4e3234
morenodiosimar-dev <morenodiosimar@gmail.com>
2026-02-11 10:35:46 -0400

    Otra vez

--- ZChat/server.js
+++ ZChat/server.js
@@ -71,8 +71,10 @@ app.get("/api/devolver_usuario", (req, res) => {
     });
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ===============================
